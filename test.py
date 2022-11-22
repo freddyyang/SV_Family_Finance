746d3e400dc12e4a948f0ecfa460d6d1a05357f9
Freddy Yang <freddyyang@outlook.com>
2022-11-21 23:17:53 -0800

    testing

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 class Test:
-    def test(self, arg):
-        print(arg)
+    def __init__(self, print):
+        self.print = print
+
+    def print_something(self):
+        print(self.print)
 
 if __name__ == '__main__':
-    test = Test()
-    test.test("Hello World!")
+    hello_world = "Hello World!"
+    test = Test(hello_world)
+    test.print_something()
