a843ad018986fbfe70abd1a288066d925ef2dee4
Freddy Yang <freddyyang@outlook.com>
2022-11-22 00:15:57 -0800

    added Contribute Calculator

---
+++ services/Calculators/ContributeCalculator.py
@@ -0,0 +1,11 @@
+from common.Interfaces.Calculators import Calculator
+
+class ContributeCalculator(Calculator):
+    """ Calculator for monthly contribution """
+
+    def __init__(self) -> None:
+        #TODO: implement class
+        pass
+
+    def calculate(self):
+        pass
